7136cc628ee526cc9da280a464ed11718bbbbd5c
Jaison Justus <jaison.justus@bookmyshow.com>
2014-08-30 20:18:30 +0530

    Improve robustness

--- cockpit.sh
+++ cockpit.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-. ~/.cockpit
+CONFIG=~/.cockpit
+
+echo -e "\nCOCKPIT** \npimping developer terminal workspace, by Team Vicious"
+echo -e "-------"
+
+checkConfigFile() {
+  found=1
+  if [[ ! -f $CONFIG ]];
+  then
+    found=0
+  fi
+
+  return $found
+}
+
 
 args=("$@")
 
@@ -21,24 +35,38 @@ parseConfig() {
   execute=""
   settingsName=${args[1]}
   settings="$settingsName[@]"
-  
   for setting in "${!settings}"
   do
-    echo $setting
     tabName="${setting%%:*}"
     tabCmd="${setting##*:}"
 
     execute+=($fullscreen $tab --title="${tabName}" -e "bash -c '$tabCmd';bash")
   done
-  
-  gnome-terminal "${execute[@]}"
+ echo ${#execute[@]} 
+  if [ ${#execute[@]} -eq 1 ]
+  then
+    fault=1;
+  else
+    gnome-terminal "${execute[@]}"
+  fi
 }
 
 loadCockpit() {
-  config=${args[1]}
+  if checkConfigFile
+  then
+    echo -e "\nFile Not Found!.\nCreated a new file at ~/.cockpit add your cockpit configuration and enjoy."
+    echo -e  '\n\t eg: work( "vim:cd ~/project;vim") \n'
+    echo "" >> ${CONFIG} 
+    exit 0
+  fi
+
+ . $CONFIG
+ config=${args[1]}
   
   parseConfig
 }
+
+fault=0;
 
 for cmd in "${COMMANDS[@]}"
 do
@@ -50,5 +78,10 @@ do
     eval ${value}
   fi
 done
+
+if [ $fault == 1 ] 
+then
+  echo "Cockpit not found. Please confirm the at ~/.cockpit"
+fi
 
 exit 0
